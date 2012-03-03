f617773171814d86d47121d58d9be5dcbce17021
Mustafa Ramadhan <mustafa.ramadhan@lxcenter.org>
2012-03-04 01:19:09 +0700

    just follow kloxo 6.1.12 code

--- kloxo/httpdocs/htmllib/lib/linuxlib.php
+++ kloxo/httpdocs/htmllib/lib/linuxlib.php
@@ -1,7 +1,7 @@
-<?php
-
+<?php 
 include_once "htmllib/phplib/lib/linuxcorelib.php";
 include_once "htmllib/lib/linuxfslib.php";
+
 
 function os_isSelfSystemUser()
 {
@@ -11,13 +11,10 @@ function os_isSelfSystemUser()
 function os_isSelfSystemOrLxlabsUser()
 {
 	$uid = posix_getuid();
-
 	if ($uid === 0) {
 		return true;
 	}
-
 	$pwd = posix_getpwuid($uid);
-
 	if ($pwd['name'] === 'lxlabs') {
 		return true;
 	}
@@ -27,28 +24,25 @@ function os_getUptime()
 {
 	$v = trim(file_get_contents("/proc/uptime"));
 	$vv = strtilfirst($v, " ");
-
 	return trim($vv);
 }
 
 function os_getLoadAvg($flag = false)
 {
-	$v = trim(file_get_contents("/proc/loadavg"));
+	$v =  trim(file_get_contents("/proc/loadavg"));
 	$v = trimSpaces($v);
 	$vv = explode(" ", $v);
 
-	if ($flag) {
-		return $vv[2];
-	}
+	if ($flag) { return $vv[2]; }
 
-	return "{$vv[0]} {$vv[1]} {$vv[2]}";
+	return  "{$vv[0]} {$vv[1]} {$vv[2]}";
 }
 
 function os_fix_fstab()
 {
 	$list = lfile_trim("/etc/fstab");
 
-	foreach ($list as $l) {
+	foreach($list as $l) {
 		$nl = trimSpaces($l);
 
 		$n = explode(" ", $nl);
@@ -57,28 +51,23 @@ function os_fix_fstab()
 			$out[] = $l;
 			continue;
 		}
-
 		if (!csa($n[3], "usrquota")) {
 			$mount[] = $n[1];
 			$n[3] = "$n[3],usrquota,grpquota";
 		}
-
 		$o = implode("\t", $n);
 		$out[] = $o;
 	}
 
 	$out = implode("\n", $out);
 	$out .= "\n";
-
-	//	dprint($out);
-
+	dprint($out);
 	lfile_put_contents("/etc/fstab", $out);
-
-	//	dprintr($mount);
-
-	foreach ($mount as $m) {
+	dprintr($mount);
+	foreach($mount as $m) {
 		system("mount $m -o remount");
 	}
+
 }
 
 function os_set_quota($username, $disk)
@@ -92,8 +81,7 @@ function os_set_quota($username, $disk)
 	}
 
 	// Issue #680 - Too high inode count in Kloxo quota - set 0 that unlimited
-	//	$inode = $disk * 500;
-
+//	$inode = $disk * 500;
 	$inode = 0;
 	lxshell_return("setquota", "-u", $username, $disk, $disk, $inode, $inode, "-a");
 }
@@ -101,7 +89,6 @@ function os_set_quota($username, $disk)
 function os_createUserQuota()
 {
 	os_fix_fstab();
-
 	system("quotacheck -afmuvg");
 	system("quotaon -auvg");
 }
@@ -109,15 +96,11 @@ function os_createUserQuota()
 function os_get_home_dir($user)
 {
 	$pwd = posix_getpwnam($user);
-
 	return $pwd['dir'];
 }
 
 function os_get_allips()
 {
-
-	$iplist = array();
-
 	// the trick for apache when behind adsl/modem
 
 	if (lxfile_exists("/home/ipalloc/webiplist")) {
@@ -127,42 +110,35 @@ function os_get_allips()
 		// minimum data ipv4
 		$out = file_get_contents("/home/ipalloc/webiplist");
 		$list = explode("\n", $out);
-
-		foreach ($list as $l) {
+		foreach($list as $l) {
 			$l = trim($l);
 			$l = str_replace("\"", "", $l);
 			$l = str_replace("'", "", $l);
 			$u = explode(",", $l);
-			$t = trim($u[0]);
-			if (!$t) {
-				continue;
-			}
-			$iplist[] = $t;
+			$t =  trim($u[0]);
+			if (!$t) { continue; }
+			$iplist[] = $t;		
 		}
-	} else {
+	}
+	else {
 	/*
 		// get ip from ifconfig
 		$out = lxshell_output("ifconfig");
 		$list = explode("\n", $out);
-
 		foreach($list as $l) {
 			$l = trim($l);
-
 			if (!csa($l, "inet addr:")) {
 				continue;
 			}
-
 			$ip = strfrom($l, "inet addr:");
 			$ip = strtilfirst($ip, " ");
-
 			if (csb($ip, "127.0")) {
 				continue;
 			}
-
 			$iplist[] = $ip;
 		}
-	 */
-		//	$iplist = getIPs_from_ifconfig(false);
+	*/
+	//	$iplist = getIPs_from_ifconfig(false);
 
 		$iplist = getIPs_from_ifcfg();
 	}
@@ -175,11 +151,11 @@ function os_get_allips()
 // and modified for except 127.0.0.1
 function getIPs_from_ifconfig($withV6 = true)
 {
-	preg_match_all('/inet' . ($withV6 ? '6?' : '') . ' addr: ?([^ ]+)/', `ifconfig`, $ips);
+	preg_match_all('/inet'.($withV6 ? '6?' : '').' addr: ?([^ ]+)/', `ifconfig`, $ips);
 
 	$a = $ips[1];
 
-	foreach ($a as $k => $v) {
+	foreach($a as $k => $v) {
 		if ($v == '127.0.0.1') {
 			unset($a[$k]);
 		}
@@ -192,42 +168,48 @@ function getIPs_from_ifconfig($withV6 = true)
 // MR - mimic from getCurrentIps() on ipaddress__redhatlib.php
 function getIPs_from_ifcfg()
 {
-	/*
-	  $p = '/etc/sysconfig/network-scripts/';
-	  $l = lscandir($p);
+/*
+	$p = '/etc/sysconfig/network-scripts/';
+	$l = lscandir($p);
 
-	  foreach($l as $t => $f) {
-		  if (stristr($f, "ifcfg-")) {
-			  if ($f === 'ifcfg-lo') { continue; }
-
-			  $c = file_get_contents($p.$f);
-			  $a = explode("\n", $c);
-
-			  foreach($a as $k => $v) {
-				  if (stristr($v, "IPADDR=")) {
-					  $i = explode("=", $v);
-					  $r[] = trim($i[1]);
-				  }
-			  }
-		  }
-	  }
-
-	  return $r;
- */
+	foreach($l as $t => $f) {
+		if (stristr($f, "ifcfg-")) {
+			if ($f === 'ifcfg-lo') { continue; }
+			$c = file_get_contents($p.$f);
+			$a = explode("\n", $c);
+			foreach($a as $k => $v) {
+				if (stristr($v, "IPADDR=")) {
+					$i = explode("=", $v);
+					$r[] = trim($i[1]);
+				}
+			}
+		}
+	}
+	
+	return $r;
+*/
 	// instead mimic function, call function directly
 
 	global $gbl, $sgbl, $login, $ghtml;
 
-	$iplist = array();
+	$driverapp = $gbl->getSyncClass(null, null, 'ipaddress');
+//	print($driverapp);
 
-	$list = Ipaddress__Redhat::getCurrentIps();
+	if ($driverapp === 'redhat') {
+		$list = Ipaddress__Redhat::getCurrentIps();
+	}
 
-	foreach ($list as $k => $v) {
-		if ($v['ipaddr'] === '127.0.0.1') {
-			continue;
+	$iplist = array(); // Initialize return value
+	if(!empty($list)) {
+		foreach($list as $k => $v) {
+			// Check ipaddr index
+			$ip_address = isset($v['ipaddr']) ? $v['ipaddr'] : NULL;
+			
+			// Skip localhost IP or empty values
+			if ($ip_address !== '127.0.0.1' && !empty($ip_address)) { 
+				$iplist[] = $ip_address;
+			}
 		}
-
-		$iplist[] = $v['ipaddr'];
 	}
 
 	return $iplist;
@@ -246,7 +228,6 @@ function os_enable_user($username)
 function os_kill_process_user($username)
 {
 	$uid = os_get_uid_from_user($username);
-
 	if ($uid) {
 		lxshell_return("pkill", "-u", $uid);
 		lxshell_return("pkill", "-9", "-u", $uid);
@@ -261,9 +242,10 @@ function os_create_default_slave_driver_db()
 	slave_save_db("driver", $a);
 }
 
+
 function os_fix_lxlabs_permission()
 {
-	global $gbl, $sgbl, $login, $ghtml;
+	global $gbl, $sgbl, $login, $ghtml; 
 	lxfile_mkdir("__path_program_root/session");
 	lxfile_unix_chown_rec("__path_program_root", "lxlabs");
 	lxfile_unix_chmod_rec("__path_program_root/sbin/", "0755");
@@ -271,12 +253,12 @@ function os_fix_lxlabs_permission()
 	lxfile_unix_chmod("__path_program_etc", "0700");
 	lxfile_unix_chmod("__path_program_root/log", "0700");
 	lxfile_unix_chmod("__path_program_root/session", "0700");
-
 	// prevent php warning when file exist - already since 6.1.7
 	if (!lxfile_exists("/usr/bin/lphp.exe")) {
 		lxfile_symlink("__path_php_path", "/usr/bin/lphp.exe");
 	}
 }
+
 
 function os_userdel($name)
 {
@@ -309,8 +291,8 @@ function os_create_system_user($basename, $password, $id, $shell, $dir = "/tmp")
 			throw new lxexception("could_not_create_user", '', $name);
 		}
 	}
-
 	return $name;
+
 }
 
 function os_service_manage($serv, $act)
@@ -320,8 +302,7 @@ function os_service_manage($serv, $act)
 
 function os_create_program_service()
 {
-	global $gbl, $sgbl, $login, $ghtml;
-
+	global $gbl, $sgbl, $login, $ghtml; 
 	$pgm = $sgbl->__var_program_name;
 
 	$pgminit = "__path_program_htmlbase/htmllib/filecore/$pgm.init.program";
@@ -336,35 +317,31 @@ function os_create_program_service()
 	lxfile_unix_chmod("/etc/init.d/$pgm", "0755");
 }
 
+
 function os_is_arch_sixfour()
 {
-	if (!lxfile_exists("/proc/xen")) {
-		$arch = trim(`arch`);
-
-		return $arch === 'x86_64';
-	} else {
-		$q = lfile_get_contents("/etc/rpm/platform");
-
-		if ($q === "i686-redhat-linux") {
-			return false;
-		}
-
-		return true;
+    if (!lxfile_exists("/proc/xen")) {
+	$arch = trim(`arch`);
+	return $arch === 'x86_64';
+    } else {
+	$q = lfile_get_contents("/etc/rpm/platform");
+	if ($q === "i686-redhat-linux") {
+	    return false;
 	}
+	return true;
+    }
 }
 
 function os_is_php_six_four()
 {
-	$v = lxshell_output("rpm -q --queryformat '%{ARCH}' php");
-	$v = trim($v);
-
-	return ($v === "x86_64");
+    $v = lxshell_output("rpm -q --queryformat '%{ARCH}' php");
+    $v = trim($v);
+    return ($v === "x86_64");
 }
 
 function os_restart_program()
 {
-	global $gbl, $sgbl, $login, $ghtml;
-
+	global $gbl, $sgbl, $login, $ghtml; 
 	$pgm = $sgbl->__var_program_name;
 	// We just need to kill the main server, and leave the wrapper alone.
 	exec_with_all_closed("/etc/init.d/$pgm lxrestart");
@@ -375,31 +352,32 @@ function os_get_network_gateway()
 	$res = lxshell_output("route", "-n");
 	$re = explode("\n", $res);
 
-	foreach ($re as $r) {
+	foreach($re as $r) {
 		if (csb($r, "0.0.0.0")) {
 			$r = trimSpaces($r);
 			$o = explode(" ", $r);
 			return trim($o[1]);
 		}
 	}
-
 	return null;
+
 }
 
 function os_get_default_ethernet()
 {
+
 	$res = lxshell_output("route", "-n");
 	$re = explode("\n", $res);
 
-	foreach ($re as $r) {
+	foreach($re as $r) {
 		if (csb($r, "0.0.0.0")) {
 			$r = trimSpaces($r);
 			$o = explode(" ", $r);
 			return trim($o[7]);
 		}
 	}
-
 	return null;
+
 }
 
 function os_getCpuNum()
@@ -408,19 +386,17 @@ function os_getCpuNum()
 
 	$i = 0;
 
-	foreach ($list as $l) {
+	foreach($list as $l) {
 		if (csa($l, "processor")) {
 			$i++;
 		}
 	}
-
 	return $i;
-}
 
+}
 function os_get_hostname()
 {
 	$v = `hostname`;
-
 	return trim($v);
 }
 
@@ -428,9 +404,8 @@ function os_get_user_from_uid($uid)
 {
 	$pwd = posix_getpwuid($uid);
 
-	if ($pwd['name']) {
+	if ($pwd['name']) 
 		return $pwd['name'];
-	}
 
 	return $uid;
 }
@@ -439,9 +414,8 @@ function os_get_uid_from_user($user)
 {
 	$pwd = posix_getpwnam($user);
 
-	if ($pwd['uid']) {
+	if ($pwd['uid']) 
 		return $pwd['uid'];
-	}
 	// If the user doesn't exist return a very large number.
 	return 10000000;
 }
@@ -450,10 +424,8 @@ function os_get_gid_from_user($user)
 {
 	$pwd = posix_getpwnam($user);
 
-	if ($pwd['gid']) {
+	if ($pwd['gid']) 
 		return $pwd['gid'];
-	}
-
 	// If the user doesn't exist return a very large number.
 	return 10000000;
 }
@@ -475,6 +447,7 @@ function os_get_commandname($pid)
 	$cmd = explode("\0", $cmd);
 	return $cmd[1];
 }
+
 
 // If pcntl is enabled, then sigterm is automatically defined. Silll
 @ define('SIGTERM', 15);
@@ -498,8 +471,9 @@ function os_killpid($pid)
 	posix_kill($pid, SIGKILL);
 }
 
+
 function os_set_path()
 {
-	global $gbl, $sgbl, $login, $ghtml;
+	global $gbl, $sgbl, $login, $ghtml; 
 	putenv("PATH=/sbin/:/usr/sbin/:/bin/:/usr/bin:/usr/local/bin/:/usr/local/sbin:$sgbl->__path_program_root/bin:$sgbl->__path_program_root/sbin:");
 }
