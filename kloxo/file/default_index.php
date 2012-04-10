ce5560184f767fe2a7ddabbbe8906f7dc4404daf
Mustafa Ramadhan <mustafa.ramadhan@lxcenter.org>
2012-04-10 12:23:16 +0700

    set absolute url for images (may problem with nginx-proxy if relative url)

--- kloxo/file/default_index.php
+++ kloxo/file/default_index.php
@@ -63,19 +63,19 @@
 	<table class="header">
 		<tr>
 			<td valign="top" width="100%"><img class="logo" 
-				src="images/logo.png" height="75" alt="hosting-logo"></td>
+				src="/images/logo.png" height="75" alt="hosting-logo"></td>
 			<td>
 				<table class="content">
 					<tr>
 						<td><a href="http://lxcenter.org/" 
 							title="Go to LxCenter website"><img class="logo" 
-							src="images/lxcenter.png" alt="lxcenter-logo" 
+							src="/images/lxcenter.png" alt="lxcenter-logo" 
 							width="120" height="35"></a></td>
 					</tr>
 					<tr>
 						<td><a href="http://lxcenter.org/software/kloxo/" 
 							title="Go to Kloxo website"><img class="logo" 
-							src="images/kloxo.png" alt="kloxo-logo" 
+							src="/images/kloxo.png" alt="kloxo-logo" 
 							width="120" height="27"></a></td>
 					</tr>
 				</table>
@@ -89,7 +89,7 @@
 		<tr>
 			<td width="50">&nbsp;</td>
 			<td valign="top"><?php include_once $incfile; ?></td>
-			<td width="280" valign="center"><img src="images/disableskeletonbg.gif"></td>
+			<td width="280" valign="center"><img src="/images/disableskeletonbg.gif"></td>
 		</tr>
 	</table>
 
