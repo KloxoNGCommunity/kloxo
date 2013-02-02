fee74062bb640ee3897b17fad384d25a1ea9ac9b
mustafaramadhan <mustafa@bigraf.com>
2013-02-03 02:23:15 +0700

    remove unwanted file(s); remove fixweb process on FixIPAddress because double process with other step of cleanup

--- kloxo/bin/fixIpAddress.php
+++ kloxo/bin/fixIpAddress.php
@@ -1,12 +1,8 @@
-<?php 
+<?php
 
-include_once "htmllib/lib/include.php"; 
+include_once "htmllib/lib/include.php";
 
 initProgram('admin');
 
 $server = $login->getfromList('pserver', 'localhost');
 $server->getandwriteipaddress();
-
-// MR -- fix issue when start/restart kloxo will be delete /home/<webserver>/conf/domains contents
-lxshell_return("sh", "/script/fixweb", "--nolog");
-
