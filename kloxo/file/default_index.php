7c76021cd97b9e1015f077dcf6290a73c91a1041
mustafaramadhan <mustafa@bigraf.com>
2013-02-12 17:07:32 +0700

    new face for Kloxo-MR (with logo)

--- kloxo/file/default_index.php
+++ kloxo/file/default_index.php
@@ -21,6 +21,7 @@
 		}
 	}
 ?>
+
 <html>
 
 <head>
@@ -31,68 +32,81 @@
 
 <?php if(isset($incfile2)) { include_once $incfile2 ; } ?>
 
-	<style type="text/css">
-		body {
-			font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
-			font-size: 8pt; font-weight: 100; background: #ddeeff; margin: 0;
-		}
+<style>
+body {
+	font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
+	font-size: 8pt;
+	font-weight: 100;
+	background-image:url('./images/abstract.jpg');
+	background-color:#cccccc;
+	margin: 0;
+}
+a {
+	text-decoration: none;
+}
+img {
+	border: 0;
+}
 
-		a { text-decoration: none; }
+img.logo {
+	margin: 5px;
+	padding: 0;
+}
 
-		img { border: 0; }
+table.header {
+	border-collapse: collapse;
+	border-spacing: 0;
+	width: 100%;
+	font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
+	font-size: 8pt;
+	font-weight: 100;
+}
 
-		img.logo { margin: 5px; padding: 0; }
+table.content {
+	border-collapse: collapse;
+	border-spacing: 0;
+	width: 100%;
+	/* height: 100%; */
+	font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
+	font-size: 8pt;
+	font-weight: 100%;
+}
 
-		table.header {
-			border-collapse: collapse; border-spacing: 0;
-			background-color: #66aadd; width: 100%;
-			font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
-			font-size: 8pt; font-weight: 100;
-		}
+table.content_body td {
+	border-collapse: collapse;
+	border: 1px dashed #cccccc;
+	font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
+	font-size: 10pt;
+	color: #444444;
+	padding:10px;
+	spacing:0;
+}
 
-		table.content {
-			border-collapse: collapse; border-spacing: 0; width: 100%;
-			font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
-			font-size: 8pt; font-weight: 100;
-		}
-	</style>
-</head>
+table.content_title td {
+	border-collapse: collapse;
+	border: 0;
+	font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
+	font-size: 12pt;
+	color: #336699;
+}
+
+</style>
 
 <body>
 
-	<table class="header">
-		<tr>
-			<td valign="top" width="100%"><img class="logo" 
-				src="./images/logo.png" height="75" alt="hosting-logo"></td>
-			<td>
-				<table class="content">
-					<tr>
-						<td><a href="http://lxcenter.org/" 
-							title="Go to LxCenter website"><img class="logo" 
-							src="./images/lxcenter.png" alt="lxcenter-logo" 
-							width="120" height="35"></a></td>
-					</tr>
-					<tr>
-						<td><a href="http://lxcenter.org/software/kloxo/" 
-							title="Go to Kloxo website"><img class="logo" 
-							src="./images/kloxo.png" alt="kloxo-logo" 
-							width="120" height="27"></a></td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="2" bgcolor="#000000">&nbsp;</td>
-		</tr>
-	</table>
-	<table class="content">
-		<tr>
-			<td width="50">&nbsp;</td>
-			<td valign="top"><?php include_once $incfile; ?></td>
-			<td width="280" valign="center"><img src="./images/disableskeletonbg.gif"></td>
-		</tr>
-	</table>
-
+<table class="header">
+	<tr>
+		<td width="100%"><img class="logo" src="./images/logo.png" height="75" alt="hosting-logo"></td>
+		<td><a href="http://mratwork.com/work/" title="Go to Kloxo-MR website"><img class="logo" src="./images/kloxo-mr.png" alt="kloxo-mr-logo" height="75"></a></td>
+	</tr>
+</table>
+<table class="content">
+	<tr>
+		<td width="50">&nbsp;</td>
+		<td valign="middle" width="100%"><?php include_once $incfile; ?></td>
+		<td width="50">&nbsp;</td>
+	</tr>
+</table>
 </body>
 
 </html>
@@ -104,4 +118,3 @@
 		include_once "./custom-index.php";
 	}
 ?>
-
--- kloxo/file/disable_inc.php
+++ kloxo/file/disable_inc.php
@@ -1,21 +1,19 @@
 	<br><br>
 	<div align="center">
-		<table width="600" cellspacing="0" cellpadding="0">
+		<table width="600" class="content_header">
 			<tr>
 				<td><br />
-				<font size="3" face="arial" color="#336699"><strong>This is the 
-				Kloxo Disable Page</strong></font> </td>
+				<font size="3" face="arial" color="#336699"><strong>This is  
+				Kloxo-MR Disable Page</strong></font> </td>
 			</tr>
 		</table>
 		<br />
-		<table width="600" cellspacing="0" cellpadding="5" bordercolor="#111111" border="0" style="border-collapse: collapse; border: 1px dashed #cccccc;">
+		<table width="600" class="content_body">
 			<tr>
 				<td width="100%" bgcolor="#F5F5F5">
-				<p><font size="2" face="arial" color="#444444">If you are seeing 
-				this page, it means that web has been DISABLED. </font></p>
-				<p><font size="2" face="arial" color="#444444">Please contact the 
-				adminstrator...<br />
-				</font></p>
+				<p>If you are seeing this page, it means that web has been DISABLED. </p>
+				<p>Please contact the 
+				adminstrator...<br /></p>
 				</td>
 			</tr>
 		</table>
