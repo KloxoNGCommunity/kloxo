98820d5264dfa6522da7cfacbd7baf2c80e59a95
mustafaramadhan <mustafa@bigraf.com>
2013-03-10 12:34:13 +0700

    fix/optimize mariadb-to-mysql and mysql-to-mariadb script

--- kloxo/bin/fix/mysql-to-mariadb.php
+++ kloxo/bin/fix/mysql-to-mariadb.php
@@ -8,39 +8,43 @@ $mysqlbranch = getRpmBranchInstalled('mysql');
 
 echo "*** Change MySQL to MariaDB - begin ***\n";
 
-echo "- Fix Service List\n";
-exec("sh /script/fix-service-list >/dev/null 2>&1");
+system("yum clean all");
+system("sh /script/fix-service-list");
+echo "\n";
 
 if (strpos($mysqlbranch, "MariaDB") !== false) {
-	echo "- Already '{$mysqlbranch}' installed\n";
+	echo "* Already '{$mysqlbranch}' installed\n";
 } elseif (strpos($mysqlbranch, "mysql") !== false) {
 
 	exec("yum list|grep MariaDB", $out, $ret);
 	
-	if (!$out) {
+	if ($ret) {
 		echo "- No repo for MariaDB.\n";
 		echo "  Open '/etc/yum.repos.d/kloxo-mr.repo and change 'enable=0' to 'enable=1'\n";
 		echo "  under [kloxo-mr-mariadb32] for 32bit OS or [kloxo-mr-mariadb64] for 64bit OS\n";
 	} else {
 		
-		exec("rpm -qa|grep {$mysqlbranch}", $out2, $ret);
+		$out2 = shell_exec("rpm -qa|grep {$mysqlbranch}");
 
-		exec("cp -f /etc/my.cnf /etc/my.cnf._bck_ >/dev/null 2>&1");
+		$arr = explode("\n", $out2);
 
 		echo "- Remove MySQL packages\n";
-		foreach ($out2 as &$o) {
+		foreach ($arr as &$o) {
 			if (strpos($o, "-mysql") !== false) { continue; }
 			if (strpos($o, "mysqlclient") !== false) { continue; }
-			exec("rpm -e {$o} --nodeps >/dev/null 2>&1");
+			system("rpm -e {$o} --nodeps");
 		}
 
-		echo "- Install MariaDB\n";
-		exec("yum install MariaDB-server MariaDB-client MariaDB-compat MariaDB-common -y >/dev/null 2>&1");
+		exec("cp -f /etc/my.cnf /etc/my.cnf._bck_");
 
-		exec("cp -f /etc/my.cnf._bck_ /etc/my.cnf.d/my.cnf >/dev/null 2>&1");
+		echo "- Install MariaDB\n";
+		system("yum install MariaDB-server MariaDB-client MariaDB-compat MariaDB-common -y");
+
+		system("cp -f /etc/my.cnf._bck_ /etc/my.cnf.d/my.cnf");
 
 		echo "- Restart MariaDB\n";
-		exec("service mysql restart");
+		system("chkconfig mysql on");
+		system("service mysql restart");
 	}
 } else {
 	echo "- No MySQL or MariaDB installed\n";
