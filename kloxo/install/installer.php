19f690fb91a45a8d1ea2f3a88d88fd4c4a2c9e68
mustafaramadhan <mustafa@bigraf.com>
2013-08-07 00:45:29 +0700

    add 'make' install when install kloxo (djbdns need it)

--- kloxo/install/installer.php
+++ kloxo/install/installer.php
@@ -346,7 +346,7 @@ function kloxo_install_step1()
 		$phpbranch = getPhpBranch();
 
 		// MR -- xcache, zend, ioncube, suhosin and zts not default install
-		$packages = array("tnef", "which", "gcc", "cpp", "gcc-c++", "zip", "unzip", "curl", "autoconf", "automake",
+		$packages = array("tnef", "which", "gcc", "cpp", "gcc-c++", "zip", "unzip", "curl", "autoconf", "automake", "make",
 			"libtool", "openssl-devel", "pure-ftpd", "yum-protectbase", "yum-plugin-replace", "crontabs",
 			"make", "glibc-static", "net-snmp", "tmpwatch", "rkhunter", "quota",
 			"{$phpbranch}", "{$phpbranch}-mbstring", "{$phpbranch}-mysql", "{$phpbranch}-pear",
