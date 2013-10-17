afc417018f50dacd480bd3359e98d98c9f4b5cfd
mustafaramadhan <mustafa@bigraf.com>
2013-10-17 21:59:29 +0700

    fix default slavedb driver

--- kloxo/install/installer.php
+++ kloxo/install/installer.php
@@ -518,8 +518,8 @@ function kloxo_install_step2()
 	}
 
 	if (!file_exists("{$kloxopath}/etc/slavedb/driver")) {
-		$driverdata = 'O:6:"Remote":1:{s:4:"data";a:3:{s:3:"web";s:6:"apache";' .
-			's:4:"spam";s:10:"bogofilter";s:3:"dns";s:4:"bind";}}';
+		$driverdata = 'O:6:"Remote":1:{s:4:"data";a:4:{s:3:"web";s:6:"apache";' .
+			's:4:"spam";s:10:"bogofilter";s:3:"dns";s:4:"bind";s:8:"webcache";s:4:"none";}}';
 		system("echo '{$driverdata}' > {$kloxopath}/etc/slavedb/driver");
 	}
 
