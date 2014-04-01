4f067d0b39a60ad193982d6dd0caf5661c10ed0a
mustafaramadhan <mustafa@bigraf.com>
2014-04-01 14:37:40 +0700

    add missing kloxo-php-fpm.conf; fix php-cli.sh for php52s

--- kloxo/init/php52s/php-cli.sh
+++ kloxo/init/php52s/php-cli.sh
@@ -2,7 +2,7 @@
 
 php_rc='/opt/php52s/custom'
 php_scan='/opt/php52s/etc/php.d'
-php_prog='/opt/php52s/usr/bin/php'
+php_prog='/opt/php52s/bin/php'
 
 #export PHPRC=$php_rc
 export PHP_INI_SCAN_DIR=$php_scan
