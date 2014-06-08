8926814cced35ad2566172e8fc7c71eb56ed104c
mustafaramadhan <mustafa@bigraf.com>
2014-06-08 09:00:03 +0700

    upload file for php-fpm conf/init/sh

---
+++ kloxo/init/php52m/php-fpm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+php_rc='/opt/php52m/custom/php.ini'
+php_scan='/opt/php52m/etc/php.d'
+php_prog='/opt/php52m/usr/sbin/php-fpm'
+
+#export PHPRC=$php_rc
+export PHP_INI_SCAN_DIR=$php_scan
+
+exec $php_prog -c $php_rc $*
