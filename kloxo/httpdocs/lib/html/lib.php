51f2a9f978e54d3e3c559dfb7a4fa03415c6c05f
mustafaramadhan <mustafa@bigraf.com>
2015-01-17 18:05:36 +0700

    fix wrong logic for mysql service

--- kloxo/httpdocs/lib/html/lib.php
+++ kloxo/httpdocs/lib/html/lib.php
@@ -913,7 +913,7 @@ function check_if_port_on($port)
 	$list = explode("||", $port);
 
 	foreach ($list as $k => $v) {
-		if (stppos($v, '.sock') !== false) {
+		if (strpos($v, '.sock') !== false) {
 			// unix socket -> /var/lib/mysql/mysql.sock for mysql
 			$socket = fsockopen('unix://{$v}', '-1', $errno, $errstr, 5);
 
@@ -8654,3 +8654,28 @@ function setRemoveHttpdocsSymlink($nolog)
 	exec("sh /script/remove-httpdocs-symlink");
 }
 
+/*
+
+define("ENCRYPTION_KEY", "!@#$%^&*");
+
+/**
+ * Returns an encrypted & utf8-encoded
+ */
+function encrypt($pure_string, $encryption_key) {
+	$iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv);
+	return $encrypted_string;
+}
+
+/**
+ * Returns decrypted original string
+ */
+function decrypt($encrypted_string, $encryption_key) {
+	$iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, $encrypted_string, MCRYPT_MODE_ECB, $iv);
+	return $decrypted_string;
+}
+
+*/
