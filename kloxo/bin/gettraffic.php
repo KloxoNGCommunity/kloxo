baf24d4e99481c892e1a370e8895bef9e565587d
mustafaramadhan <mustafa@bigraf.com>
2015-07-08 03:31:05 +0700

    mod gettraffic (add try-catch for web and mail stats); change 'check_vhost_docroot=false' for suphp (trouble for ~user url if 'true'); add update-ckeditor script

--- kloxo/bin/gettraffic.php
+++ kloxo/bin/gettraffic.php
@@ -9,7 +9,9 @@ log_log("get_traffic", $val);
 function gettraffic_main()
 {
 	global $argc, $argv;
+
 	$list = parse_opt($argv);
+
 	if (isset($list['delete-table']) && $list['delete-table'] === 'yes') {
 		print("clearing Traffic Table\n");
 		clearTrafficTable();
@@ -34,16 +36,16 @@ function filltraffictable()
 	$login->loadAllObjects('client');
 	$clist = $login->getList('client');
 	$t="";
- // Fake domain to store the time the last stats finding was done.
 
+ // Fake domain to store the time the last stats finding was done.
 	$laccessdom = new Domain(null, null, '__last_access_domain_');
+
 	try {
 		$laccess = $laccessdom->getFromList('domaintraffic', '__last_access_domain_');
 	} catch (exception $e) {
 		dprint("not getting\n");
 		$laccess = null;
 	}
-
 
 	if (!$laccess) {
         $laccess = new Domaintraffic(null, null, '__last_access_domain_');
@@ -63,10 +65,12 @@ function filltraffictable()
 
 	foreach($clist as $c) {
 		$domlist = $c->getList('domain');
+
 		foreach((array)$domlist as $domain) {
 			if (!$domain->isDomainVirtual()) {
 				continue;
 			}
+
 			$web = $domain->getObject('web');
 			$mmail= $domain->getObject('mmail');
 			$globaldomlist[$domain->nname] = $domain;
@@ -76,7 +80,7 @@ function filltraffictable()
 	}
 
 	$flag = 0;
-	
+
 	if($oldtime == 0) {
 		// 8 days back
 		$oldtime  =  @ mktime(00, 01, 00, date("n"), date("j") - 10, date("Y"));
@@ -84,10 +88,10 @@ function filltraffictable()
 		//$oldtime  =  mktime( 00 , 01, 00 , 1 ,1, date("Y"));
 		$flag = 1;
 	}
-	
+
 	// $newtime =   mktime( 00 , 01, 00 , date("n")  , date("j")  ,date("Y"));
 
-    $newtime = time();
+	$newtime = time();
 
 	$old = $oldtime;
 	$new = $newtime;
@@ -98,6 +102,7 @@ function filltraffictable()
 			$timearray[0] = $newtime . "-" . $old; 
 			$newtime = $old;
 		}
+
 		$j=0;
 
 		for($i = $newtime; $i >= $oldtime ; $i-= (24 *  60 * 60)) {
@@ -111,13 +116,16 @@ function filltraffictable()
 		if($flag != 1) {
 			$timearray[] =  "$new-$oldtime";
 		}
+
 		$timearray = array_reverse($timearray);
 
 		foreach($timearray as $t1) {
 			$t = explode("-", $t1);
 			$newtime = $t[0];
 			$oldtime = $t[1];
+
 			if ($newtime - $oldtime < 4 * 60 * 60) { continue; }
+
 			$o = @ strftime("%c", $oldtime);
 			$n = @ strftime("%c", $newtime);
 			print("\n\n$o to $n\n\n"); 
@@ -135,57 +143,68 @@ function filltraffictable()
 		dprint("Less than a day:");
 		dprint("\n\n\n\n");
 	}
-
 } 
  
 function get_mailaccountlist($m)
 {
-	 $list = array();
-	  foreach($m as $v) {
+	$list = array();
+
+	foreach($m as $v) {
 		$mac = $v->getList('mailaccount');
+
 		if ($mac) {
 			$list = array_merge($list, get_namelist_from_objectlist($mac));
 		}
 	}
-  return $list;
-}
 
+	return $list;
+}
 
 function findtraffic($weblist, $mmaillist, $globaldomlist, $oldtime, $newtime)
 {
-
 	global $gbl, $login, $ghtml; 
 
 	$web_usage = null;
 
 	$gen = $login->getObject('general')->generalmisc_b;
 	$webstatsprog = $gen->webstatisticsprogram;
+
 	if (!$webstatsprog) { $webstatsprog = "awstats"; }
 
-	foreach($weblist as $k => $dlist) {
-		$list = null;
-		foreach($dlist as $d) {
-			$rt = new Remote();
-			$rt->iisid = $d->getObject('web')->iisid;
-			$rt->nname = $d->nname;
-			$rt->customer_name = $d->getRealClientParentO()->getPathFromName('nname');
-			$rt->priv = new Priv(null, null, $d->nname);
-			$rt->priv->awstats_flag = $d->priv->awstats_flag;
-			$rt->stats_username = $d->getObject('web')->stats_username;
-			$rt->stats_password = $d->getObject('web')->stats_password;
-			$rt->remove_processed_stats = $d->getObject('web')->remove_processed_stats;
-			$list[$d->nname] = $rt;
+	try {
+		foreach($weblist as $k => $dlist) {
+			$list = null;
+
+			foreach($dlist as $d) {
+				$rt = new Remote();
+				$rt->iisid = $d->getObject('web')->iisid;
+				$rt->nname = $d->nname;
+				$rt->customer_name = $d->getRealClientParentO()->getPathFromName('nname');
+				$rt->priv = new Priv(null, null, $d->nname);
+				$rt->priv->awstats_flag = $d->priv->awstats_flag;
+				$rt->stats_username = $d->getObject('web')->stats_username;
+				$rt->stats_password = $d->getObject('web')->stats_password;
+				$rt->remove_processed_stats = $d->getObject('web')->remove_processed_stats;
+				$list[$d->nname] = $rt;
+			}
+
+			$driverapp = $gbl->getSyncClass(null, $k, 'web');
+			$web_usaget = rl_exec_get(null, $k, array("webtraffic", 'findTotaltrafficwebUsage'), array($driverapp, $webstatsprog, $list, $oldtime, $newtime)); 
+			$web_usage = lx_array_merge(array($web_usage , $web_usaget));
 		}
-		$driverapp = $gbl->getSyncClass(null, $k, 'web');
-		$web_usaget = rl_exec_get(null, $k, array("webtraffic", 'findTotaltrafficwebUsage'), array($driverapp, $webstatsprog, $list, $oldtime, $newtime)); 
-		$web_usage = lx_array_merge(array($web_usage , $web_usaget));
+	} catch (exception $e) {
+		print($e->getMessage(). "\n");
 	}
 
-	foreach($mmaillist as $k => $m) {
-		$mlist = get_namelist_from_objectlist($m);
-		$driverapp = $gbl->getSyncClass(null, $k, 'mmail');
-		$mailusaget  = rl_exec_get(null, $k, array("mailtraffic", 'findTotalmailQuota'), array($driverapp, $mlist, $oldtime, $newtime)); 
-		$mailusage = lx_array_merge(array($mailusaget, $mailusage));
+	try {
+		foreach($mmaillist as $k => $m) {
+			$mlist = get_namelist_from_objectlist($m);
+			$driverapp = $gbl->getSyncClass(null, $k, 'mmail');
+			$mailusaget  = rl_exec_get(null, $k, array("mailtraffic", 'findTotalmailQuota'), array($driverapp, $mlist, $oldtime, $newtime)); 
+			$mailusage = lx_array_merge(array($mailusaget, $mailusage));
+		}
+	} catch (exception $e) {
+		print($e->getMessage(). "\n");
 	}
 
 	try {
@@ -202,7 +221,9 @@ function findtraffic($weblist, $mmaillist, $globaldomlist, $oldtime, $newtime)
 	//dprintr($web_usage);
 	//dprintr($mailusage);
 	//dprintr($ftpusage);
+
 	$res="";
+
 	foreach($globaldomlist as $d) {
 		$res['nname'] = "$d->nname:$oldtime:$newtime";
 		$domt = new Domaintraffic(null, null, $res['nname']);
@@ -221,7 +242,6 @@ function findtraffic($weblist, $mmaillist, $globaldomlist, $oldtime, $newtime)
 		$domt->was();
 	}
 } 
-
 
 function testFunc()
 {
