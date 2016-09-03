6457a7c442888eeccf20a4bac810c0df9c6be067
Mustafa Ramadhan <mustafa@bigraf.com>
2016-09-03 10:21:37 +0700

    add missing fixdnsaddrecord script; mod fixssl also update ssl in kloxo database; mod fixssl also check update_ssl in /etc/cron.d; add fix-cron-ssl for running ssl in every month

--- kloxo/bin/fix/fixssl.php
+++ kloxo/bin/fix/fixssl.php
@@ -29,7 +29,7 @@ foreach($ilist as $b) {
 	}
 }
 
-print("- Processing for 'program' ssl files\n");
+print("- Processing for 'program' SSL files\n");
 
 if ((!is_link("{$kloxo_etc_path}/program.pem")) ||
 		((is_link("{$kloxo_etc_path}/program.pem")) && (!file_exists("{$kloxo_etc_path}/program.pem")))) {
@@ -49,10 +49,49 @@ $slist = $login->getList('sslcert');
 $sslpath = "/home/kloxo/ssl";
 $lepath = "/etc/letsencrypt";
 
+$apath = "/root/.acme.sh/";
+$spath = "/root/.startapi.sh";
+
+print("- Updating for 'domain' SSL files\n");
+
 foreach($slist as $b) {
 	if (csb($b->parent_clname, 'web-')) {
 		$dom = $b->nname;
-		print("- Processing for '{$dom}' ssl files\n");
+		$type = $b->add_type;
+
+		if ($type === 'letsencrypt') {
+			if (file_exists("{$apath}/{$dom}/ca.cer")) {
+				print("  * Letsencrypt SSL file for '{$dom}'\n");
+				$b->text_key_content = file_get_contents("{$apath}/{$dom}/{$dom}.key");
+				$b->text_crt_content = file_get_contents("{$apath}/{$dom}/{$dom}.cer");
+				$b->text_ca_content = file_get_contents("{$apath}/{$dom}/ca.cer");
+
+				$b->setUpdateSubaction();
+
+				$b->write();
+			}
+		} elseif ($type === 'startapi') {
+			if (file_exists("{$spath}/{$dom}/{$dom}.pem")) {
+				print("  * StartAPI SSL file for '{$dom}'\n");
+				$b->text_key_content = file_get_contents("{$spath}/{$dom}/{$dom}.key");
+				$b->text_crt_content = file_get_contents("{$spath}/{$dom}/{$dom}.cer");
+				$b->text_ca_content = file_get_contents("{$spath}/{$dom}/ca.cer");
+
+				$b->setUpdateSubaction();
+
+				$b->write();
+			}
+		}
+	}
+
+	$b->was();
+}
+
+print("- Processing for 'domain' SSL files\n");
+
+foreach($slist as $b) {
+	if (csb($b->parent_clname, 'web-')) {
+		$dom = $b->nname;
 
 		// MR -- remove old data for domain in letsencrypt data
 		exec("'rm' -rf {$lepath}/{live,archive,renewal}/{$dom}-*");
@@ -66,6 +105,7 @@ foreach($slist as $b) {
 
 			foreach ($list as $k => $v) {
 				if (file_exists("{$sslpath}/{$par}.{$v}")) {
+					print("  * SymLink SSL file for '{$dom}' from '{$par}'\n");
 					exec("ln -sf {$sslpath}/{$par}.{$v} {$sslpath}/{$dom}.{$v}");
 				}
 			}
@@ -83,9 +123,14 @@ foreach($slist as $b) {
 				}
 			}
 
+			print("  * SSL file for '{$dom}'\n");
+
 		//	exec("echo '{$keyc}\n{$crtc}\n{$cac}' >{$sslpath}/{$dom}.pem");
 			file_put_contents("{$sslpath}/{$dom}.pem", $pemc);
 		}
 	}
+
+	$b->was();
 }
+
 
