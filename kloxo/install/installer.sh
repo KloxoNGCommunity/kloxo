585ba0f3852dbe944add96d23630bdc9309c5da8
Mustafa Ramadhan <mustafa@bigraf.com>
2017-02-25 16:17:57 +0700

    add sudo install in installer.sh/setup.sh and fix-urgent (run by cleanup)

--- kloxo/install/installer.sh
+++ kloxo/install/installer.sh
@@ -153,7 +153,7 @@ cd /
 
 #yum clean all
 
-yum -y install wget zip unzip yum-utils yum-priorities yum-plugin-replace vim-minimal subversion curl
+yum -y install wget zip unzip yum-utils yum-priorities yum-plugin-replace vim-minimal subversion curl sudo
 yum remove -y bind* nsd* pdns* mydns* yadifa* maradns djbdns* mysql* mariadb* MariaDB* php* \
 		httpd-* mod_* httpd24u-* mod24u_* nginx* lighttpd* varnish* squid* trafficserver \
 		*-toaster postfix exim libmhash
