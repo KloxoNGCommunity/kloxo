48e3468cb786d405b69baf4e99da2b99a8249192
John Pierce <john@luckytanuki.com>
2018-02-01 10:32:48 +1100

    Change version Update release rpm location

--- kloxo/install/installer.sh
+++ kloxo/install/installer.sh
@@ -18,24 +18,25 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
-# MRatWork - KloxoNG dev Installer
+# KloxoNG - KloxoNG dev Installer
 #
 # Version: 1.0 (2013-01-11 - by Mustafa Ramadhan <mustafa@bigraf.com>)
 # Version: 1.1 (2018-01-27 - by Dionysis Kladis <dkstiler@gmail.com>)
 #
 
-$main_repo_url="https://github.com/mustafaramadhan/rpms/raw/master/mratwork/release/neutral/noarch"
-$main_release_rpm="mratwork-release-0.0.1-1.noarch.rpm"
+$main_repo_url="https://github.com/KloxoNGCommunity/KloxoNG-rpms/tree/master/kloxong/release/neutral/noarch"
+$main_release_rpm="kloxong-release-0.1.0-1.noarch.rpm"
 $rpm_main_pck='kloxong'
 
-if [ "$(rpm -qa mratwork-release)" == "" ] ; then
+if [ "$(rpm -qa kloxong-release)" == "" ] ; then
 	cd /tmp
 	rpm -ivh $main_repo_url/$main_release_rpm >/dev/null 2>&1
 	rpm -ivh $main_release_rpm >/dev/null 2>&1
-	yum update mratwork-release -y >/dev/null 2>&1
+	yum update kloxong-release -y >/dev/null 2>&1
 
 	'mv' -f /etc/yum.repos.d/lxcenter.repo /etc/yum.repos.d/lxcenter.nonrepo >/dev/null 2>&1
 	'mv' -f /etc/yum.repos.d/kloxo-mr.repo /etc/yum.repos.d/kloxo-mr.nonrepo >/dev/null 2>&1
+        'mv' -f /etc/yum.repos.d/kloxong.repo /etc/yum.repos.d/kloxong.nonrepo >/dev/null 2>&1
 else
 	yum update mratwork-release -y >/dev/null 2>&1
 fi
